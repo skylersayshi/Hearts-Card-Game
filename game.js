016a3f11e37d028302cb2741b35ddae318694f2e
giantnerd123 <payneskyler@yahoo.com>
2022-03-08 22:23:27 -0800

    cleaned up functions

--- game.js
+++ game.js
@@ -40,29 +40,13 @@ function makeNewDeck(){
 
 const deck = new Deck();
 
-// console.log(deck.cards);
-// let computer1Cards = [];
-// let computer2Cards = [];
-// let computer3Cards = [];
-// let playerCards = [];
-
 function dealCards(){
     deck.shuffleCards();
     computer1Cards = deck.cards.splice(0, 13);
-        // console.log(computer1Cards.length);
     computer2Cards = deck.cards.splice(0, 13);
-        // console.log(computer2Cards.length);
     computer3Cards = deck.cards.splice(0, 13);
-        // console.log(computer3Cards.length);
     playerCards = deck.cards.splice(0, 13);
-    //     console.log(playerCards.length);
-
-    // // while(deck.cards.length){
-    // for(let i = 0; i<deck.cards.length; i++);{
-    //     computer1Cards.push(deck.cards.slice(i,i+13))
-    //     computer2Cards.push(deck.cards.slice(i+14,i+13))
-        // console.log(deck.cards.splice(0,13, computer1Cards, computer2Cards, computer3Cards, playerCards));
  };
 
-
 dealCards();
+console.log(computer1Cards.length);
