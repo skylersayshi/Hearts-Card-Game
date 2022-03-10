cd8fbf078856c63f150c39445f2e6d4dd4695422
giantnerd123 <payneskyler@yahoo.com>
2022-03-10 01:27:30 -0800

    sorted cards in hand

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 // CONSTRUCTING THE DECK OF CARDS
 
-const suits = ["♠", "♦", "♥", "♣"];
-const values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+const suits = ["♥", "♠" , "♦", "♣"];
+const values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"];
 
 class Deck {
     constructor(cards = makeNewDeck()) {
@@ -37,10 +37,26 @@ function makeNewDeck(){
 
 const deck = new Deck();
 
+function insertionSort(inputArr) {
+    let n = inputArr.length;
+        for (let i = 1; i < n; i++) {
+            // Choosing the first element in our unsorted subarray
+            let current = inputArr[i];
+            // The last element of our sorted subarray
+            let j = i-1; 
+            while ((j > -1) && (current < inputArr[j])) {
+                inputArr[j+1] = inputArr[j];
+                j--;
+            }
+            inputArr[j+1] = current;
+        }
+    return inputArr;
+}
+
 function dealCards(){
     deck.shuffleCards();
     computer1Cards = deck.cards.splice(0, 13);
-        // console.log(computer1Cards);
+        computer1Cards.sort((a,b)=> (a.suit.localeCompare(b.suit) || a.value - b.value ));
         for(let i = 0; i<computer1Cards.length; i++){
         c1Hand.innerHTML += (computer1Cards[i].value + computer1Cards[i].suit)
         }
@@ -54,9 +70,12 @@ function dealCards(){
         }
     playerCards = deck.cards.splice(0, 13);
         for(let i = 0; i<computer1Cards.length; i++){
+
             playerHand.innerHTML += (playerCards[i].value + playerCards[i].suit)
         }
  };
+
+ 
 
 // Cache the DOM and Add Event Listeners
 
@@ -67,3 +86,7 @@ c3Hand = document.querySelector('#hand-c3');
 playerHand = document.querySelector('#hand-player');
 
 dealButton.addEventListener('click', dealCards);
+
+//adjust deal function to create element for every item within hand so that each item can be individually selected
+//
+
