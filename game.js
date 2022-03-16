0f537e1bb7760914b57a3cfd37258da55c4b6fd8
giantnerd123 <payneskyler@yahoo.com>
2022-03-15 18:57:13 -0700

    made first round playable

--- game.js
+++ game.js
@@ -48,7 +48,7 @@ pot1 = document.querySelector('.pot1');
 pot2 = document.querySelector('.pot2');
 pot3 = document.querySelector('.pot3');
 playerPot = document.querySelector('.pot-player');
-pointsInPot = document.querySelector('.pot-points')
+pointsInPot = document.querySelector('.pot-points');
 
 deck.shuffleCards();
 
@@ -78,6 +78,7 @@ function dealCards(){
         }
         playerHand.appendChild(createPlayerHand());
     }
+firstRound();
 };
 
 dealButton.addEventListener('click', dealCards);
@@ -94,6 +95,7 @@ function firstRound(){
     for(let i = 0; i<computer2Cards.length; i++){
         if(computer2Cards[i].suit === "♣" && computer2Cards[i].value === "2"){
             pot2.innerHTML += computer2Cards[i].value + computer2Cards[i].suit;
+            pot.push(computer2Cards[i]);
             computer2Cards.splice(i,1);
         }
     }
@@ -101,6 +103,7 @@ function firstRound(){
     for(let i = 0; i<computer3Cards.length; i++){
         if(computer3Cards[i].suit === "♣" && computer3Cards[i].value === "2"){
             pot3.innerHTML += computer3Cards[i].value + computer3Cards[i].suit;
+            pot.push(computer3Cards[i]);
             computer3Cards.splice(i,1);
         }
     }
@@ -108,146 +111,65 @@ function firstRound(){
     for(let i = 0; i<playerCards.length; i++){
         if(playerCards[i].suit === "♣" && playerCards[i].value === "2"){
             playerPot.innerHTML += playerCards[i].value + playerCards[i].suit;
+            pot.push(playerCards[i]);
             playerCards.splice(i,1);
         }
     }
 
-    // do {
-    //     if(computer1Cards.length<computer2Cards.length){
-    //         let cardPlayed = getRandomCard(computer2Cards).value + getRandomCard(computer2Cards).suit;
-    //         pot.push(cardPlayed);
-    //         pot2.innerHTML+=cardPlayed.value + cardPlayed.suit;
-    //         computer2Cards.splice(cardPlayed,1);
-    //     }
-    //     if(computer2Cards.length<computer3Cards.length){
-    //         let cardPlayed = getRandomCard(computer3Cards).value + getRandomCard(computer3Cards).suit;
-    //         pot.push(cardPlayed);
-    //         pot3.innerHTML+=cardPlayed.value + cardPlayed.suit;
-    //         computer3Cards.splice(cardPlayed,1);
-    //     }
-    //     if(computer3Cards.length<playerCards.length){
-    //         let cardPlayed = getRandomCard(playerCards).value + getRandomCard(playerCards).suit;
-    //         pot.push(cardPlayed);
-    //         playerPot.innerHTML+=cardPlayed.value + cardPlayed.suit;
-    //         playerCards.splice(cardPlayed,1);
-    //     }
-    //     addPoints();
-    //     return;
-        
-    // } while(pot.length<4);
-}
-
-firstRound();
-
-function playRound(){
+    playCardFromHand();
+    addPoints();
 };
 
-playRound();
+function playCardFromHand(){
+    while(pot.length<4) {
+        if(computer1Cards.length<computer2Cards.length){
+            let cardPlayed = getRandomCard(computer2Cards);
+            pot.push(cardPlayed); 
+            pot2.innerHTML+=cardPlayed.value + cardPlayed.suit;
+            computer2Cards.splice(cardPlayed,1);
+            // c2Hand.innerHTML = computer2Cards.length;
+            console.log(pot);
+        }
+        if(computer2Cards.length<computer3Cards.length){
+            let cardPlayed = getRandomCard(computer3Cards);
+            pot.push(cardPlayed);
+            pot3.innerHTML+=cardPlayed.value + cardPlayed.suit;
+            computer3Cards.splice(cardPlayed,1);
+            // c3Hand.innerHTML = computer3Cards.length;
+        }
+        if(computer3Cards.length<playerCards.length){
+            let cardPlayed = getRandomCard(playerCards);
+            pot.push(cardPlayed);
+            playerPot.innerHTML+=cardPlayed.value + cardPlayed.suit;
+            playerCards.splice(cardPlayed,1);
+            // playerHand.innerHTML = playerCards.length;
+            //to be modified to allow player to play game
 
-function addPoints(){
-    for(let i = 0; i<pot.length; i++){
-        if(pot[i].suit === "♥"){
-            potPoints++;
+        }
+        if(playerCards.length<computer1Cards.length){
+            let cardPlayed = getRandomCard(computer1Cards);
+            pot.push(cardPlayed);
+            pot1.innerHTML+=cardPlayed.value + cardPlayed.suit;
+            computer1Cards.splice(cardPlayed,1);
+            // c1Hand.innerHTML = computer1Cards.length;
         }
     }
 };
-function givePoints(){
-    
-}
+
+function addPoints(){ //needs work
+    for(let i = 0; i<pot.length; i++){
+        if(pot[i].value === "♥"){
+            potPoints++;
+            pointsInPot.innerHTML = potPoints;
+        }
+    }
+    console.log(potPoints);
+
+};
 
 function getRandomCard(player){
     const randomCard = Math.floor(Math.random() * player.length);
     const item = player[randomCard];
+    // return item.value + item.suit;
     return item;
-}
-
-playerPot.innerHTML += getRandomCard(playerCards).value + getRandomCard(playerCards).suit;
-// Cache the DOM and Add Event Listeners
-
-// dealButton = document.querySelector('.deal');
-// c1Hand = document.querySelector('#hand-c1');
-// c2Hand = document.querySelector('#hand-c2');
-// c3Hand = document.querySelector('#hand-c3');
-// playerHand = document.querySelector('#hand-player');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function dealCards(){
-//     deck.shuffleCards();
-//     computer1Cards = deck.cards.splice(0, 13);
-//         // computer1Cards.sort((a,b)=> (a.suit.localeCompare(b.suit) || a.value - b.value ));
-//         for(let i = 0; i<computer1Cards.length; i++){
-//         c1Hand.innerHTML += (computer1Cards[i].value + computer1Cards[i].suit)
-//         }
-//     computer2Cards = deck.cards.splice(0, 13);
-//         for(let i = 0; i<computer1Cards.length; i++){
-//             c2Hand.innerHTML += (computer2Cards[i].value + computer2Cards[i].suit)
-//         }
-//     computer3Cards = deck.cards.splice(0, 13);
-//         for(let i = 0; i<computer1Cards.length; i++){
-//             c3Hand.innerHTML += (computer3Cards[i].value + computer3Cards[i].suit)
-//         }
-//     playerCards = deck.cards.splice(0, 13);
-//         for(let i = 0; i<computer1Cards.length; i++){
-//             playerCards.sort((a,b)=> (a.suit.localeCompare(b.suit) || a.value - b.value ));
-//             function createPlayerHand(cards){
-//                 let card = document.createElement('div');
-//                 card.classList.add('card');
-//                 card.innerHTML = playerCards[i].value + playerCards[i].suit;
-//                 return card;
-//             }
-//             playerHand.appendChild(createPlayerHand());
-//         }
-//  };
-    
+};
