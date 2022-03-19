a6697dd7955ec1e1699cdf34f88260e5052aac35
giantnerd123 <payneskyler@yahoo.com>
2022-03-19 01:43:51 -0700

    Finished V1

--- game.js
+++ game.js
@@ -65,6 +65,9 @@ c2Points = document.querySelector('.c2-points');
 c3Points = document.querySelector('.c3-points');
 playerPoints = document.querySelector('.player-points');
 messageBoard = document.querySelector('.message-board');
+history1 = document.querySelector('.history1');
+history2 = document.querySelector('.history2');
+history3 = document.querySelector('.history3');
 
 cardChoice = document.querySelectorAll('.card')
 
@@ -93,19 +96,20 @@ pPointsJS = 0;
 
 function dealCards(){
     for(let i = 0; i<computer1Cards.length; i++){
-        c1Hand.innerHTML += (computer1Cards[i].value + computer1Cards[i].suit)
+        c1Hand.innerHTML += "__"
     }
     for(let i = 0; i<computer2Cards.length; i++){
-        c2Hand.innerHTML += (computer2Cards[i].value + computer2Cards[i].suit)
+        c2Hand.innerHTML += "__"
     }
     for(let i = 0; i<computer3Cards.length; i++){
-        c3Hand.innerHTML += (computer3Cards[i].value + computer3Cards[i].suit)
+        c3Hand.innerHTML += "__"
     }
 };
 
 ////TESTING__________________________
 //add delay
 var delayInMilliseconds = 500;
+var delay3Seconds = 3000;
 
 for(let i = 0; i<playerCards.length; i++){
     function createPlayerHand(cards){
@@ -113,6 +117,7 @@ for(let i = 0; i<playerCards.length; i++){
         card.classList.add('card');
         card.setAttribute("id", `${i}`)
         card.innerHTML = faceCards(playerCards[i].value) + playerCards[i].suit;
+        suitColor(card)
         return card;
     }
     playerHand.appendChild(createPlayerHand(playerCards[i].value + playerCards[i].suit));
@@ -122,8 +127,8 @@ cardChoice.forEach(card => {
     //waits for user input
     card.addEventListener('click', () =>{ //plays user card to the pot
         pot.push(playerCards[card.id]);
-        playerPot.innerHTML+=card.innerHTML
-        // suitColor(playerPot)
+        playerPot.innerHTML=card.innerHTML
+        suitColor(playerPot)
         if(pot.length === 1){
             initialSuit = playerCards[card.id].suit;
         }
@@ -148,6 +153,7 @@ function firstRound(){ //no problems
     for(let i = 0; i<computer1Cards.length; i++){
         if(computer1Cards[i].suit === "♣" && computer1Cards[i].value === "2"){
             pot1.innerHTML += computer1Cards[i].value + computer1Cards[i].suit;
+            suitColor(pot1)
             pot.push(computer1Cards[i]);
             c1Deleted.push(computer1Cards[i]);
             initialSuit = computer1Cards[i].suit;
@@ -158,6 +164,7 @@ function firstRound(){ //no problems
     for(let i = 0; i<computer2Cards.length; i++){
         if(computer2Cards[i].suit === "♣" && computer2Cards[i].value === "2"){
             pot2.innerHTML += computer2Cards[i].value + computer2Cards[i].suit;
+            suitColor(pot2)
             pot.push(computer2Cards[i]);
             c2Deleted.push(computer2Cards[i]);
             initialSuit = computer2Cards[i].suit;
@@ -168,6 +175,7 @@ function firstRound(){ //no problems
     for(let i = 0; i<computer3Cards.length; i++){
         if(computer3Cards[i].suit === "♣" && computer3Cards[i].value === "2"){
             pot3.innerHTML += computer3Cards[i].value + computer3Cards[i].suit;
+            suitColor(pot3)
             pot.push(computer3Cards[i]);
             c3Deleted.push(computer3Cards[i]);
             initialSuit = computer3Cards[i].suit;
@@ -203,7 +211,8 @@ function playCardFromHand(){ //MAJOR PROBLEMS
         if(playerCards.length<computer1Cards.length && pot.length<4 && computer1Cards.length>=playerCards.length){
             let cardPlayed = getRandomCard(computer1Cards);
             pot.push(cardPlayed);
-            setTimeout(function() {pot1.innerHTML += faceCards(cardPlayed.value) + cardPlayed.suit},delayInMilliseconds);
+            pot1.innerHTML += faceCards(cardPlayed.value) + cardPlayed.suit
+            suitColor(pot1)
             // suitColor(pot1);
             c1Deleted.push(cardPlayed);
             spliceCard(cardPlayed, computer1Cards);
@@ -213,7 +222,8 @@ function playCardFromHand(){ //MAJOR PROBLEMS
             // console.log('condition 2');
             let cardPlayed = getRandomCard(computer2Cards);
             pot.push(cardPlayed); 
-            setTimeout(function() {pot2.innerHTML += faceCards(cardPlayed.value) + cardPlayed.suit},delayInMilliseconds);
+            pot2.innerHTML += faceCards(cardPlayed.value) + cardPlayed.suit
+            suitColor(pot2)
             c2Deleted.push(cardPlayed);
             spliceCard(cardPlayed, computer2Cards);
             // computer2Cards.splice(cardPlayed,1);
@@ -224,7 +234,8 @@ function playCardFromHand(){ //MAJOR PROBLEMS
             // console.log('condition 3');
             let cardPlayed = getRandomCard(computer3Cards);
             pot.push(cardPlayed);
-            setTimeout(function() {pot3.innerHTML += faceCards(cardPlayed.value) + cardPlayed.suit},delayInMilliseconds);
+            pot3.innerHTML += faceCards(cardPlayed.value) + cardPlayed.suit
+            suitColor(pot3)
             c3Deleted.push(cardPlayed);
             // computer3Cards.splice(cardPlayed,1);
             spliceCard(cardPlayed, computer3Cards);
@@ -302,7 +313,7 @@ function givePoints(){ //creates next hand starter and adds points
     for(let i = 0; i<potValues.length; i++){
         for(let j = 0; j<c1Deleted.length; j++){
             if (potValues[i] == highestCard && c1Deleted[j] == pot[i]){
-                messageBoard.innerHTML = faceCards(pot[i].value) + pot[i].suit + " takes this hand";
+                messageBoard.innerHTML = "Computer 1 takes this hand with " + faceCards(pot[i].value) + pot[i].suit;
                 c1PointsJS = c1PointsJS + potPoints;
                 c1Points.innerHTML = c1PointsJS;
                 nextHandStarter = "c1"
@@ -310,21 +321,21 @@ function givePoints(){ //creates next hand starter and adds points
                 // console.log('COMPUTER 1 POINTS: ' + )
             }
             if (potValues[i] == highestCard && c2Deleted[j] == pot[i]){
-                messageBoard.innerHTML = faceCards(pot[i].value) + pot[i].suit + " takes this hand";
+                messageBoard.innerHTML = "Computer 2 takes this hand with " + faceCards(pot[i].value) + pot[i].suit;
                 c2PointsJS = c2PointsJS + potPoints;
                 c2Points.innerHTML = c2PointsJS;
                 nextHandStarter = "c2"
                 // return nextHandStarter
             }
             if (potValues[i] == highestCard && c3Deleted[j] == pot[i]){
-                messageBoard.innerHTML = faceCards(pot[i].value) + pot[i].suit + " takes this hand";
+                messageBoard.innerHTML = "Computer 3 takes this hand with " + faceCards(pot[i].value) + pot[i].suit;;
                 c3PointsJS = c3PointsJS + potPoints;
                 c3Points.innerHTML = c3PointsJS;
                 nextHandStarter = "c3"
                 // return nextHandStarter
             }
             if (potValues[i] == highestCard && pDeleted[j] == pot[i]){
-                messageBoard.innerHTML = faceCards(pot[i].value) + pot[i].suit + " takes this hand";
+                messageBoard.innerHTML = "You took this hand with " + faceCards(pot[i].value) + pot[i].suit + ". PLAY NEXT CARD";;
                 pPointsJS = pPointsJS + potPoints;
                 playerPoints.innerHTML = pPointsJS;
                 nextHandStarter = "player"
@@ -346,7 +357,8 @@ function nextHand(){ //includes new hand parameters
     if(nextHandStarter === "c1"){
         let cardPlayed1 = getRandomCard2(computer1Cards);
             pot.push(cardPlayed1);
-            setTimeout(function(){pot1.innerHTML+=faceCards(cardPlayed1.value) + cardPlayed1.suit},delayInMilliseconds);
+            pot1.innerHTML+=faceCards(cardPlayed1.value) + cardPlayed1.suit
+            suitColor(pot1)
             c1Deleted.push(cardPlayed1);
             initialSuit = cardPlayed1.suit;
             // computer1Cards.splice(cardPlayed1,1);
@@ -360,7 +372,8 @@ function nextHand(){ //includes new hand parameters
     if(nextHandStarter === "c2"){
         let cardPlayed2 = getRandomCard2(computer2Cards);
             pot.push(cardPlayed2); 
-            setTimeout(function(){pot2.innerHTML+=faceCards(cardPlayed2.value) + cardPlayed2.suit},delayInMilliseconds);
+            pot2.innerHTML+=faceCards(cardPlayed2.value) + cardPlayed2.suit
+            suitColor(pot2)
             c2Deleted.push(cardPlayed2);
             initialSuit = cardPlayed2.suit;
             spliceCard(cardPlayed2,computer2Cards);
@@ -369,7 +382,8 @@ function nextHand(){ //includes new hand parameters
     if(nextHandStarter === "c3"){
         let cardPlayed3 = getRandomCard2(computer3Cards);
             pot.push(cardPlayed3);
-            setTimeout(function(){pot3.innerHTML+=faceCards(cardPlayed3.value) + cardPlayed3.suit},delayInMilliseconds);
+            pot3.innerHTML+=faceCards(cardPlayed3.value) + cardPlayed3.suit
+            suitColor(pot3)
             c3Deleted.push(cardPlayed3);
             initialSuit = cardPlayed3.suit;
             // computer3Cards.splice(cardPlayed3,1);
@@ -447,6 +461,18 @@ function finishRound(){
     console.log('player length:' + playerCards.length)
         addPoints()
         givePoints()
+        history1.innerHTML = pot1.innerHTML
+        suitColor(history1)
+        history2.innerHTML = pot2.innerHTML
+        suitColor(history2)
+        history3.innerHTML = pot3.innerHTML
+        suitColor(history3)
+        
+        pot1.innerHTML = "";
+        pot2.innerHTML = "";
+        pot3.innerHTML = "";
+        playerPot.innerHTML = "";
+
         newHandParameters()
         nextHand()
         // console.log(playerCards.length)
@@ -470,8 +496,10 @@ function suitColor(item){
     }
 }
 
-function delay(item){
-    setTimeout(function(){
-        item
-    },delayInMilliseconds);
-}
+function winGame(){
+    if(playerCards.length === 0 && computer1Cards.length === 0 && computer2Cards.length === 0 && computer3Cards.length === 0){
+    console.log('game over');
+    }
+
+    }
+    winGame();
